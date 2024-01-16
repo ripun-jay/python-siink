011d3b297916c247980204548ce7198d5f39f5ac
ripun <ripunjaytiwari8@gmail.com>
2024-01-17 02:14:19 +0530

    added is operator

---
+++ operator/1.operator and operands.py
@@ -0,0 +1,31 @@
+# operator -- > perform mth, logical, relational operations
+
+# mathematical
+# +,  -,   *,  /,  %,  //,  **
+a = 5
+b = 7
+
+print(a+b)
+print(a*b)
+print(a/b)
+print(a-b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+
+
+# Logical/ 
+# and or not
+print(True and True and False)
+print(True or False)
+print(not True)
+
+
+
+# Assignment operator
+#  =, +=, *=, %=
+
+
+#compareition operator
+# ==,  !=,  <=, >=
---
+++ operator/is operator.py
@@ -0,0 +1,9 @@
+# is :- compare values are stroed in same location or not
+
+a= 5
+b= 5
+c = 5.0
+
+print(id(a), id(b), id(c) ,sep= "\n")
+print(a is b)
+print(a is c)
